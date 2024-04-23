a2294f4dfbe40284afbd65879d0631bdf62d26ae
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-04-23 11:18:56 +0100

    Added blank module

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Onboarding",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Onboarding",
+            targets: ["Onboarding"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Onboarding",
+            path: "Onboarding/Sources"
+        ),
+        .testTarget(
+            name: "OnboardingTests",
+            path: "Onboarding/Tests",
+            dependencies: ["Onboarding"]
+        ),
+    ]
+)

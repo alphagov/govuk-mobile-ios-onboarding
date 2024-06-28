55bf1c021b6c2720325225f0de48876ae6fc4745
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-06-28 12:32:52 +0100

    Added public init to OnboardingSlide

--- Onboarding/Sources/Onboarding/Models/OnboardingSlide.swift
+++ Onboarding/Sources/Onboarding/Models/OnboardingSlide.swift
@@ -2,7 +2,15 @@ import Foundation
 import UIKit
 
 public struct OnboardingSlide: Codable {
-    let image: String
-    let title: String
-    let body: String
+    public let image: String
+    public let title: String
+    public let body: String
+
+    public init(image: String,
+                title: String,
+                body: String) {
+        self.image = image
+        self.title = title
+        self.body = body
+    }
 }
